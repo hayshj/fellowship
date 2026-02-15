a1521c590c5ad7cf8a568041a4734a6abbe9e645
hayshj <143533420+hayshj@users.noreply.github.com>
2026-02-15 10:16:09 -0600

    Update Give.jsx

--- frontend/src/pages/Give.jsx
+++ frontend/src/pages/Give.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function Give() {
   useEffect(() => {
-    window.location.replace("https://yourgivinglink.com");
+    window.location.replace("https://churchteams.com/m/Give.asp?oID=13823&aID=YUVxR1c3S2N2SlhoTHM2bmNxUmdqUkpxa1JRdkJyUzU%3D");
   }, []);
 
   return null;
